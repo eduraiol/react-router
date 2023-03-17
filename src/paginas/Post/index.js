bf883732cf8f58682310050abdf458e12e56f969
Eduardo Raiol <marquesraiol@gmail.com>
2023-03-17 10:48:55 -0300

    Criando componente NaoEncontrado

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,13 +1,36 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from 'react';
+
+import './Post.css';
+
+import { useParams } from 'react-router-dom';
+import posts from "json/posts.json";
+import PostModelo from 'componentes/PostModelo';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
 function Post() {
-    const parametros = useParams();
+  const parametros = useParams();
 
-    console.log(parametros);
+  const post = posts.find((post) => {
+    return post.id === Number(parametros.id)
+  })
+
+  if(!post){
+    return <h1>Post não encontrado...</h1>
+  }
 
   return (
-    <h1>Post..</h1>
+    <PostModelo
+      fotoCapa={`/assets/posts/${post.id}/capa.png`}
+      titulo={post.titulo}
+    >
+      <div className="post-markdown-container">
+        <ReactMarkdown>
+         {post.texto}
+      </ReactMarkdown>
+     
+      </div>
+      
+    </PostModelo>
   )
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import PaginaPadrao from "componentes/PaginaPadrao";
 import Rodape from "componentes/Rodape";
+import NaoEncontrado from "paginas/NaoEncontrada";
 import Post from "paginas/Post";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "./componentes/Menu";
@@ -18,7 +19,7 @@ function AppRoutes() {
           <Route path="posts/:id" element={<Post />} />
         </Route>
         
-        <Route path="*" element={<div>Página não encontrada</div>} />
+        <Route path="*" element={<NaoEncontrado />} />
       </Routes>
 
       <Rodape />
